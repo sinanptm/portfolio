d3a92cd6392536851ce94748af6ad52614722293
Sinan <muhammedsinan7749@gmail.com>
2024-10-21 18:24:06 +0530

    basic setup added

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import { Metadata } from "next";
+import localFont from "next/font/local";
+import "../style/globals.css";
+
+const geistSans = localFont({
+  src: "../public/fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../public/fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Muhammed Sinan",
+  description: "sinanptm's portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
+        {children}
+      </body>
+    </html>
+  );
+}
