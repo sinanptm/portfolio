c16b8996ab48e981b821de28293635088f14578d
Sinan <muhammedsinan7749@gmail.com>
2024-10-22 21:42:57 +0530

    img changed to Image

--- components/SkillsSection.tsx
+++ components/SkillsSection.tsx
@@ -1,35 +1,22 @@
 'use client';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { skills } from "@/constants";
-import { motion } from "framer-motion";
+import Image from "next/image";
 import { useMemo } from "react";
 
 export default function SkillsSection() {
 
-  const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.1,
-      },
-    },
-  };
-
   const Skills = useMemo(()=>{
     return skills.map((skill, index) => (
       <div key={index}>
-        <img src={skill.url} alt={skill.name} className="h-6" />
+        <Image src={skill.url}  alt={skill.name} width={23} height={23} className="h-6 w-auto" />
       </div>
     ))
   },skills)
 
   return (
-    <motion.section
+    <section
       className="mb-16"
-      variants={containerVariants}
-      initial="hidden"
-      animate="visible"
     >
       <h2 className="text-3xl font-semibold text-purple-400 mb-6">Technical Skills</h2>
       <Card className="bg-gray-800/50 border-gray-700 backdrop-blur-sm hover:bg-gray-700/50 transition-all duration-300">
@@ -37,16 +24,13 @@ export default function SkillsSection() {
           <CardTitle className="text-xl text-purple-400">Skills & Technologies</CardTitle>
         </CardHeader>
         <CardContent>
-          <motion.div
+          <div
             className="flex flex-wrap gap-2"
-            variants={containerVariants}
-            initial="hidden"
-            animate="visible"
           >
             {Skills} 
-          </motion.div>
+          </div>
         </CardContent>
       </Card>
-    </motion.section>
+    </section>
   );
 }
