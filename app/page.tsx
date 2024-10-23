3330bf14ff0818437759a82493bf7737624ad00d
Sinan <muhammedsinan7749@gmail.com>
2024-10-23 22:17:57 +0530

    three js added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import HeaderSection from "@/components/HeaderSection"
 import ProfileSummary from "@/components/ProfileSummary"
 import SkillsSection from "@/components/SkillsSection"
-// import ProjectsSection from "@/components/ProjectsSection";
+import ThreeScene from "@/components/three/ThreeScene";
 
 export default function Portfolio() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-purple-900 to-gray-800 py-12 px-4 sm:px-6 lg:px-8">
         <HeaderSection />
+        <ThreeScene />
         <ProfileSummary />
         <SkillsSection />
         {/* <ProjectsSection /> */}
