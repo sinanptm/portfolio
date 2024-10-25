cf31cde5c779c162aa46a56f433f63a06854d146
Sinan <muhammedsinan7749@gmail.com>
2024-10-25 07:55:24 +0530

    Hero completed

--- components/3D/CanvasLoader.tsx
+++ components/3D/CanvasLoader.tsx
@@ -8,7 +8,7 @@ export default function CanvasLoader() {
 
   return (
     <Html center>
-      <div className="w-64 p-4 rounded-lg shadow-lg">
+      <div className="w-64 p-4 rounded-lg shadow-lg mt-32">
         <Progress value={progress} className="w-full mb-0 h-1" />
         <p className="text-center text-sm font-sm">{progress.toFixed(0)}%</p>
       </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,9 +33,9 @@ const Hero = () => {
   return (
       <section className="relative w-full h-screen mx-auto ">
         <div className={`pb-7 absolute inset-0 top-[120px] mx-auto flex flex-row items-start gap-5`}>
-          <div className="flex flex-col justify-center items-center mt-5">
-            {/* <div className="w-5 h-5 rounded-full bg-violet-600" />
-            <div className="w-1 sm:h-80 h-40 violet-gradient" /> */}
+          <div className="flex flex-col justify-center items-center mt-5 ml-5">
+            <div className="w-5 h-5 rounded-full bg-violet-600" />
+            <div className="w-1 sm:h-80 h-40 violet-gradient" />
           </div>
           <div>
             <h1 className={`font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px]`}>
@@ -47,7 +47,6 @@ const Hero = () => {
 
         <ComputersCanvas />
 
-        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center" />
       </section>
   );
 };
