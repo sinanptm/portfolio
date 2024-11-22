d4bebad1a156272f52bedb23e6f5efb51180a870
Sinan <muhammedsinan7749@gmail.com>
2024-11-22 22:33:57 +0530

    robots and sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+export default {
+    siteUrl: 'https://sinanptm.vercel.app', // Replace with your portfolio's URL
+    generateRobotsTxt: true,
+    exclude: [],
+};
