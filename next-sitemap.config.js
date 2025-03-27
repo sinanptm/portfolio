c167505f57fa7ea1ce3c83bda0e90a7781091f4e
muhammed sinan <muhammedsinan7749@gmail.com>
2025-03-27 19:42:46 +0530

    sitemap url changed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://sinanptm.vercel.app', // Replace with your portfolio's URL
+    siteUrl: 'https://muhammedsinan.space', // Replace with your portfolio's URL
     generateRobotsTxt: true,
     exclude: [],
 };
