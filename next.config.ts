600685a6380fd5fccb2a1406c2f390e146bc2d8c
muhammed sinan <muhammedsinan7749@gmail.com>
2025-10-27 10:00:02 +0530

    testing

--- next.config.ts
+++ next.config.ts
@@ -22,28 +22,6 @@ const nextConfig: NextConfig = {
     ],
     dangerouslyAllowSVG: true
   },
-  async headers() {
-    return [
-      {
-        source: "/_next/image",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/assets/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
